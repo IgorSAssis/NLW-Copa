e6bb1529975ba2aba3564942ee7d1c7bb78b17df
IgorIA <igorsemphoski@gmail.com>
2022-11-06 11:25:43 -0300

    feat: first and second class completed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
 import fastifyCors from "@fastify/cors";
+import { z } from "zod";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -12,17 +14,49 @@ async function bootstrap() {
   });
 
   await fastify.register(fastifyCors, {
-    origin: true
-  });
-
-  fastify.get("/", (request, reply) => {
-    reply.status(200).send("Hello world!");
+    origin: true,
   });
 
   fastify.get("/pools/count", async () => {
-    const count = prisma.pool.count();
+    const count = await prisma.pool.count();
 
     return { count };
+  });
+
+  fastify.get("/users/count", async () => {
+    const count = await prisma.user.count();
+
+    return { count };
+  });
+
+  fastify.get("/guesses/count", async () => {
+    const count = await prisma.guess.count();
+
+    return { count };
+  });
+
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generatedCode = new ShortUniqueId({
+      length: 6,
+    });
+    const code = String(generatedCode()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+
+    return reply.status(201).send({
+      code,
+    });
   });
 
   await fastify.listen({
